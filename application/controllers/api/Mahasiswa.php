f804dce9ec5adc7cbf38a0381598515ab944720e
Refri Rifwan <refririfwan@gmail.com>
2020-01-09 22:58:44 +0700

    auth

--- application/controllers/api/Mahasiswa.php
+++ application/controllers/api/Mahasiswa.php
@@ -12,6 +12,7 @@ class Mahasiswa extends REST_Controller
     {
         parent::__construct();
         $this->load->model('Mahasiswa_model');
+        $this->methods['index_get']['limit'] = 123;
     }
     public function index_get()
     {
